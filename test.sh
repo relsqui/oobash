3d26fe7aa77a4060f69d3216534126938e903d56
Finn Ellis <relsqui@chiliahedron.com>
2019-06-11 21:16:08 -0700

    YES IT WORKS

--- test.sh
+++ test.sh
@@ -2,13 +2,17 @@
 
 source oobash
 
-alias widget1=$(new object)
+eval $(new object widget1)
 widget1 .size = large
 widget1 .color = blue
 
-alias widget2=$(new object)
+eval $(new object widget2)
 widget2 .size = medium
 widget2 .color = green
 
-widget1 .size
-widget2 .size
+eval $(new object widget3)
+widget3 .size = $(widget1 .size)
+widget3 .color = $(widget2 .color)
+
+widget3 .size
+widget3 .color
