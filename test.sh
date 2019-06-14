102125bc02ef21be56f742ff87888ab7952266e1
Finn Ellis <relsqui@chiliahedron.com>
2019-06-14 10:00:50 -0700

    Working methods!!

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-
 source oobash
 
 eval $(new object widget)
-
-widget .intro = 'I am a widget'
-
+widget .intro = 'I am a widget!'
 widget def fancy_echo << 'EOF'
-echo "~!~ $@"
+echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
+echo "~!~ $@ ~!~"
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 EOF
 
 widget .fancy_echo $(widget .intro)
+
+
