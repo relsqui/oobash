b716f5023360244456ed37bdb39774cfef72b0b3
Finn Ellis <relsqui@chiliahedron.com>
2019-06-14 09:21:12 -0700

    Start on methods, not working yet.

--- test.sh
+++ test.sh
@@ -2,17 +2,12 @@
 
 source oobash
 
-eval $(new object widget1)
-widget1 .size = large
-widget1 .color = blue
+eval $(new object widget)
 
-eval $(new object widget2)
-widget2 .size = medium
-widget2 .color = green
+widget .intro = 'I am a widget'
 
-eval $(new object widget3)
-widget3 .size = $(widget1 .size)
-widget3 .color = $(widget2 .color)
+widget def fancy_echo << 'EOF'
+echo "~!~ $@"
+EOF
 
-widget3 .size
-widget3 .color
+widget .fancy_echo $(widget .intro)
