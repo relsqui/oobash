ef8c90022e8585a86582433902a3603aa2d3eafc
Finn Ellis <relsqui@chiliahedron.com>
2019-06-14 22:23:41 -0700

    Experiment failed but I'll stash it for safekeeping

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 source oobash
 
-eval $(new class widget)
-widget .name = 'Widget Class'
-widget def introduce << 'EOF'
+widget=$(new class)
+$widget .name = 'Widget Class'
+$widget def introduce << 'EOF'
 echo "Hi, I'm $($self .name)!"
 EOF
 
-eval $(new widget widget1)
-widget1 .name = 'Widget One'
+widget1=$(new widget)
+$widget1 .name = 'Widget One'
 
-eval $(new widget widget2)
-widget2 .name = 'Widget Two'
+widget2=$(new widget)
+$widget2 .name = 'Widget Two'
 
-widget1 .introduce
-widget2 .introduce
+$widget1 .introduce
+$widget2 .introduce
 
 
